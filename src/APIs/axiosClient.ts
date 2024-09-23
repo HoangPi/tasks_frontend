ff32c58ad2faa880182adbc10eb72f79d02704bc
Phi <voduchoangphi@gmail.com>
2024-09-23 13:32:05 +0700

    add refresh token interceptor

--- src/APIs/axiosClient.ts
+++ src/APIs/axiosClient.ts
@@ -2,7 +2,39 @@ import axios from "axios";
 
 export const AxiosClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-        Authorization: localStorage.getItem('access') || ''
+})
+
+AxiosClient.interceptors.request.use(req => {
+    if (localStorage.getItem('access')) {
+        req.headers.Authorization = `Bearer ${localStorage.getItem('access')}`
     }
+    return req
+})
+
+AxiosClient.interceptors.response.use(res => {
+    res.request.headers
+    return res
+}, async (err) => {
+    localStorage.removeItem('access')
+    if (err.response.status === 401 && err.config.headers.Authorization) {
+        if (!localStorage.getItem('refresh')) {
+            Promise.reject(err)
+        }
+        try {
+            const refreshRes = await AxiosClient.post('refresh', {
+                refresh: localStorage.getItem('refresh')
+            })
+            if(Boolean(refreshRes.data.access)){
+                localStorage.setItem('access', refreshRes.data.access)
+                err.config.headers.Authorization = `Bearer ${refreshRes.data.access}`
+                return await AxiosClient(err.config)
+            }
+        }
+        catch(e){
+            localStorage.removeItem('refresh')
+            Promise.reject(err)
+        }
+    }
+    localStorage.removeItem('refresh')
+    Promise.reject(err)
 })
